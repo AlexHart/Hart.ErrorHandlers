0e7285c14ef5ac5c1b70e417d6ed4d9c6ba0bc65
Alejandro Hartlöhner <hartformer@hotmail.com>
2018-06-12 23:22:09 +0200

    Add comments

--- Hart.ErrorHandlers/Retry/RetryExtensions.cs
+++ Hart.ErrorHandlers/Retry/RetryExtensions.cs
@@ -4,32 +4,53 @@ using System.Threading.Tasks;
 namespace Hart.ErrorHandlers.Retry
 {
 
-    //TODO: Add comments.
-
     /// <summary>
     /// Extensions for retrying functionality.
     /// </summary>
     public static class RetryExtensions
     {
 
+        /// <summary>
+        /// Wait command n milliseconds between retries.
+        /// </summary>
+        /// <param name="retryConfig"></param>
+        /// <param name="msWait"></param>
+        /// <returns></returns>
         public static RetryConfig WithMsWaitOf(this RetryConfig retryConfig, int msWait)
         {
             retryConfig.WaitBetweenRetries = TimeSpan.FromMilliseconds(msWait);
             return retryConfig;
         }
 
+        /// <summary>
+        /// Wait command for an specified timespan duration between retries.
+        /// </summary>
+        /// <param name="retryConfig"></param>
+        /// <param name="wait"></param>
+        /// <returns></returns>
         public static RetryConfig WithWaitOf(this RetryConfig retryConfig, TimeSpan wait)
         {
             retryConfig.WaitBetweenRetries = wait;
             return retryConfig;
         }
 
+        /// <summary>
+        /// Specify the number of retries.
+        /// </summary>
+        /// <param name="retryConfig"></param>
+        /// <param name="retries"></param>
+        /// <returns></returns>
         public static RetryConfig WithNumberOfRetries(this RetryConfig retryConfig, int retries)
         {
             retryConfig.MaxRetries = retries;
             return retryConfig;
         }
 
+        /// <summary>
+        /// Retry until it's succesful.
+        /// </summary>
+        /// <param name="retryConfig"></param>
+        /// <returns></returns>
         public static RetryConfig RetryUntilSuccessful(this RetryConfig retryConfig) {
             retryConfig.RetryForever = true;
             return retryConfig;
@@ -46,6 +67,13 @@ namespace Hart.ErrorHandlers.Retry
             return retryConfig;
         }
 
+        /// <summary>
+        /// Configure a fallback function
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="fallback"></param>
+        /// <returns></returns>
         public static RetryResult<T> WithFallBack<T>(this RetryResult<T> result, Func<T> fallback)
         {
             if (fallback == null)
@@ -67,6 +95,13 @@ namespace Hart.ErrorHandlers.Retry
             return result;
         }
 
+        /// <summary>
+        /// Configure an async fallback function.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="fallback"></param>
+        /// <returns></returns>
         public static async Task<RetryResult<T>> WithFallBackAsync<T>(this RetryResult<T> result, Func<Task<T>> fallback)
         {
             if (fallback == null)
@@ -90,11 +125,24 @@ namespace Hart.ErrorHandlers.Retry
             return result;
         }
 
+        /// <summary>
+        /// Helper to extract result from a task.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <returns></returns>
         public static RetryResult<T> WaitForValue<T>(this Task<RetryResult<T>> result)
         {
             return result.Result;
         }
 
+        /// <summary>
+        /// Specify a fallback value if all the tries fail.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
         public static RetryResult<T> WithFallBackValue<T>(this RetryResult<T> result, T defaultValue)
         {
             if (defaultValue == null)
@@ -116,6 +164,12 @@ namespace Hart.ErrorHandlers.Retry
             return result;
         }
 
+        /// <summary>
+        /// Configure a fallback action.
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="fallback"></param>
+        /// <returns></returns>
         public static RetryResult WithFallBack(this RetryResult result, Action fallback)
         {
             if (fallback == null)
