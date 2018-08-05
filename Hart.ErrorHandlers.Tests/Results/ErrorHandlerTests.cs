3d470fcbb58e2b531cc0d58d7c6de17db17c2a07
AlexHart <ahartlohner@gmail.com>
2018-08-05 03:30:15 +0200

    Add wrapper for func and actions to put results inside an IResult

---
+++ Hart.ErrorHandlers.Tests/Results/ErrorHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using ErrorHandlersTests;
+using Hart.ErrorHandlers.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Hart.ErrorHandlers.Tests.Results
+{
+    public class ErrorHandlerTests
+    {
+
+        [Fact]
+        public void WrapSuccessAction()
+        {
+            var result = ErrorHandler.Execute(() =>
+            {
+                Console.WriteLine("hello world");
+            });
+
+            Assert.True(result.IsOk);
+            Assert.IsType<Success>(result);
+        }
+
+        [Fact]
+        public void WrapErrorAction()
+        {
+            var result = ErrorHandler.Execute(() => throw new Exception(""));
+
+            Assert.False(result.IsOk);
+            Assert.IsType<Error>(result);
+        }
+
+        [Fact]
+        public void WrapSuccessFunc()
+        {
+            var result = ErrorHandler.Execute(() => 2 + 2);
+
+            Assert.True(result.IsOk);
+            Assert.IsType<Success<int>>(result);
+            Assert.Equal(4, result.GetSuccess());
+        }
+
+        [Fact]
+        public void WrapErrorFunc()
+        {
+            var result = ErrorHandler.Execute(() => FakeService.OutOfMemory());
+
+            Assert.False(result.IsOk);
+            Assert.IsType<Error<int>>(result);
+        }
+
+    }
+}
---
+++ Hart.ErrorHandlers/Results/ErrorHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hart.ErrorHandlers.Results
+{
+    public static class ErrorHandler
+    {
+        /// <summary>
+        /// Execute func safely and wrap the return inside an IResult of T.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fun"></param>
+        /// <returns></returns>
+        public static IResult<T> Execute<T>(Func<T> fun)
+        {
+            try
+            {
+                var funResult = fun();
+                return new Success<T>(funResult);
+            }
+            catch (Exception ex)
+            {
+                return new Error<T>(ex);
+            }
+        }
+
+        /// <summary>
+        /// Execute Action safely and wrap up the return inside an IResult.
+        /// </summary>
+        /// <param name="fun"></param>
+        /// <returns></returns>
+        public static IResult Execute(Action fun)
+        {
+            try
+            {
+                fun();
+                return new Success();
+            }
+            catch (Exception ex)
+            {
+                return new Error(ex);
+            }
+        }
+
+    }
+}
