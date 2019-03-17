1066f043a1d5db149cb83e1d82f264bdfe024494
Alejandro Hartlohner Utrera <hartformer@hotmail.com>
2019-03-17 14:53:57 +0100

    Update comments and formatting

--- Hart.Validation.Tests/Person/PersonExtensions.cs
+++ Hart.Validation.Tests/Person/PersonExtensions.cs
@@ -9,9 +9,21 @@ namespace Hart.Validation.Tests
         {
             var validationRules = new List<ValidationRule>
             {
-                new ValidationRule("always ok", () => true),
-                new ValidationRule(IdMessage, () => person.Id > 1),
-                new ValidationRule(DateMessage, () => person.Birthday.Year > 3000)
+                new ValidationRule(
+                    "always ok", 
+                    () => true),
+
+                new ValidationRule(
+                    IdMessage, 
+                    () => person.Id > 1),
+
+                new ValidationRule(
+                    DateMessage, 
+                    () => person.Birthday.Year > 3000),
+
+                new ValidationRule(
+                    "Name can't be empty",
+                    () => person.Name != string.Empty)
             };
 
             var result = validationRules.Validate();
--- Hart.Validation/ValidationRule.cs
+++ Hart.Validation/ValidationRule.cs
@@ -10,7 +10,7 @@ namespace Hart.Validation
         /// Initializes a new instance of the <see cref="T:Hart.Validation.ValidationRule"/> struct.
         /// </summary>
         /// <param name="errorMessage">Error message.</param>
-        /// <param name="validationFunction">Validation function.</param>
+        /// <param name="validationFunction">Validation function that should be true</param>
         /// <remarks>The validation function is a closure so be carefull what you pass to it if it has to be disposed fast</remarks>
         public ValidationRule(string errorMessage, Func<bool> validationFunction)
         {
